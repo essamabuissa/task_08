ca6a93b4c994bcf5875dd443a53faf453afa85a8
Essam <essamabuissa@hotmail.com>
2020-02-17 09:48:30 +0200

    Task 8 Done

--- task_08/urls.py
+++ task_08/urls.py
@@ -19,9 +19,10 @@ from restaurants import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('restaurants/list/',views.restaurant_list ,name='restaurant-list'),
     path('restaurants/detail/<int:restaurant_id>/',views.restaurant_detail ,name='restaurant-detail'),
 
@@ -32,3 +33,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
